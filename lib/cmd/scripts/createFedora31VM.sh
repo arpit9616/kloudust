1c91cf11cf2bf62067e777b697dfa84f066e356e
Rohit Kapoor <rvkapoor@tekmonks.com>
2020-09-28 15:05:30 -0400

    Added Fedora 31 support.

---
+++ lib/cmd/scripts/createFedora31VM.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+function exitFailed() {
+    echo Failed
+    exit 1
+}
+
+printf "Creating Fedora 31 VM\n"
+virt-install --name {1} --metadata title="{2}" \
+    --vcpus {3} --ram {4} \
+    --disk path=/kloudust/disks/{1}.img,size={5} \
+    --os-type linux --os-variant fedora31 \
+    --network bridge=virbr0 \
+    --graphics vnc,listen=0.0.0.0 --noautoconsole \
+    {6} \
+    --virt-type kvm
+
+printf "\n\nConnect via VNC to one of the following\n"
+PORT=`virsh vncdisplay {1} | cut -c 2-`;echo `ip route get 8.8.8.8 | head -1 | cut -d' ' -f7`:`expr 5900 + $PORT`
+echo `hostname`:`expr 5900 + $PORT`
+
+printf "\n\nVM created successfully\n"
+exit 0
